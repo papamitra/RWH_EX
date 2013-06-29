d06541781a5c3cbcfd345ef7b938d6bd856fec60
y-tsutsumi <tsutsu0928@gmail.com>
2013-06-29 16:18:31 +0900

    マージミスを修正しておいたよ．

--- ch04/exercises.hs
+++ ch04/exercises.hs
@@ -1,14 +1,19 @@
 
--- Problem 1
+-- ex1
 safeHead :: [a] -> Maybe a
 safeHead [] = Nothing
 safeHead xs = Just $ head xs
+
+-- m-oshita
+safeHead_m :: [a] -> Maybe a
+safeHead_m [] = Nothing
+safeHead_m (x:xs) = Just x
 
 safeLast :: [a] -> Maybe a
 safeLast [] = Nothing
 safeLast xs = Just $ last xs
 
-<<<<<<< HEAD
+
 -- ex2
 splitWith :: (a -> Bool) -> [a] -> [[a]]
 splitWith pred xs =
@@ -18,12 +23,8 @@ splitWith pred xs =
     (pre,suf) -> pre : (drp suf)
   where drp xs = let (pre, suf) = break pred xs
                  in splitWith pred suf
-=======
--- m-oshita
-safeHead_m :: [a] -> Maybe a
-safeHead_m [] = Nothing
-safeHead_m (x:xs) = Just x
 
+-- m-oshita
 splitWith_m :: (a -> Bool) -> [a] -> [[a]]
 splitWith_m f x = splitWith' f x [] []
   where splitWith' f [] ls rt | null ls = reverse rt
@@ -31,5 +32,3 @@ splitWith_m f x = splitWith' f x [] []
         splitWith' f (x:xs) ls rt | (f x) == True = splitWith' f xs (x:ls) rt
                                   | (f x) == False && null ls = splitWith' f xs [] rt 
                                   | otherwise = splitWith' f xs [] ((reverse ls):rt)
->>>>>>> fd178826196d964136334338370c490dc3e34469
-
