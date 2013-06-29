a303202ef305ffd0e8644572a54d6e9330677922
mind mind <minddnim@gmail.com>
2013-06-29 15:46:01 +0900

    safeHead_m, splitWith_mを作成

--- ch04/exercises.hs
+++ ch04/exercises.hs
@@ -7,3 +7,16 @@ safeHead xs = Just $ head xs
 safeLast :: [a] -> Maybe a
 safeLast [] = Nothing
 safeLast xs = Just $ last xs
+
+-- m-oshita
+safeHead_m :: [a] -> Maybe a
+safeHead_m [] = Nothing
+safeHead_m (x:xs) = Just x
+
+splitWith_m :: (a -> Bool) -> [a] -> [[a]]
+splitWith_m f x = splitWith' f x [] []
+  where splitWith' f [] ls rt | null ls = reverse rt
+                              | otherwise = reverse $ (reverse ls):rt
+        splitWith' f (x:xs) ls rt | (f x) == True = splitWith' f xs (x:ls) rt
+                                  | (f x) == False && null ls = splitWith' f xs [] rt 
+                                  | otherwise = splitWith' f xs [] ((reverse ls):rt)
